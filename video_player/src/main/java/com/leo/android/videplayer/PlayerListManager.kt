e6e774867c6adeb2ffe2b02541b7e60c9804d294
wanglg <670500876@qq.com>
2018-11-24 20:50:32 +0800

    //

--- video_player/src/main/java/com/leo/android/videplayer/PlayerListManager.kt
+++ video_player/src/main/java/com/leo/android/videplayer/PlayerListManager.kt
@@ -1,6 +1,9 @@
 package com.leo.android.videplayer
 
+import android.content.Context
 import android.net.Uri
+import android.view.ViewGroup
+import com.danikula.videocache.HttpProxyCacheServer
 import com.leo.android.videplayer.ijk.PlayerConfig
 
 class PlayerListManager {
@@ -9,8 +12,35 @@ class PlayerListManager {
     var nextPath: Uri? = null
     var preLoad: Boolean = false
     var playConfig: PlayerConfig? = null
+    var mCacheServer: HttpProxyCacheServer? = null
+    var parentView: ViewGroup? = null
+    val videoView: IjkVideoView
+
+    private constructor(context: Context) {
+        videoView = IjkVideoView(context)
+    }
 
     class Builder {
-        var target: PlayerListManager = PlayerListManager()
+        var target: PlayerListManager
+
+        constructor(context: Context) {
+            target = PlayerListManager(context)
+        }
+
+        fun attachView(parentView: ViewGroup){
+            target.parentView=parentView
+        }
+
+        fun build(): PlayerListManager {
+            return target
+        }
+
+    }
+
+    fun play() {
+        playConfig?.let {
+            videoView.setPlayerConfig(it)
+        }
+
     }
 }
