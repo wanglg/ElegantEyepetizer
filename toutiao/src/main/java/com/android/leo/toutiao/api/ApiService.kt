20dde91dfff689376341c45f565a988bdfc67115
wanglg <670500876@qq.com>
2018-12-21 01:39:10 +0800

    //视频feed流ui开发

--- toutiao/src/main/java/com/android/leo/toutiao/api/ApiService.kt
+++ toutiao/src/main/java/com/android/leo/toutiao/api/ApiService.kt
@@ -5,6 +5,7 @@ import com.android.leo.toutiao.mvp.model.response.NewsResponse
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface ApiService {
 
@@ -18,4 +19,10 @@ interface ApiService {
     @GET(GET_ARTICLE_LIST)
     fun getNewsList(@Query("category") category: String, @Query("min_behot_time") lastTime: Long,
                     @Query("last_refresh_sub_entrance_interval") currentTime: Long): Observable<NewsResponse>
+
+    /**
+     * 获取视频页的html代码
+     */
+    @GET
+    fun getVideoHtml(@Url url: String): Observable<String>
 }
